7d48f9805b98bdf5f11ba0071f921e9dde089479
unknown <chikviladze555@yahoo.com>
2024-08-26 18:58:22 +0400

    send data from child to parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -11,8 +11,20 @@ import { PostListComponent } from './post-list/post-list.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular18';
 
-  postTitle: string = 'Post1';
+  appPostTitle: string = 'Post1';
+
+  appIsLogin: boolean = false;
+
+  @ViewChild(PostListComponent) childMessage: any;
+
+  constructor() {
+    console.log('child message', this.childMessage);
+  }
+
+  ngAfterViewInit() {
+    console.log('child message2', this.childMessage);
+  }
 }
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
   imports: [],
   templateUrl: './post-list.component.html',
-  styleUrl: './post-list.component.css'
+  styleUrl: './post-list.component.css',
 })
 export class PostListComponent {
+  @Input() postListTitle: string = '';
 
+  @Input() postIsLogin: boolean = false;
+
+  childMessage: string = 'Hello from child component';
+  postCount: number = 0;
 }
